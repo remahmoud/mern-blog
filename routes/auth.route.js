e01af8e0ac0da907e49e4fccdc5e4cc35997a924
Mahmoud Ibrahiam <equalmahmoud@gmail.com>
2023-04-29 07:26:35 +0300

    update controller name

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,16 +1,16 @@
-const AuthController = require("../controller/auth.controller");
+const Controller = require("../controller/auth.controller");
 const router = require("express").Router();
 const schema = require("../utils/schema");
 const validator = require("../middleware/validator");
 const isAuth = require("../middleware/isAuth");
 
 // ---- User Data ---- //
-router.get("/me", isAuth, AuthController.getMyData);
+router.get("/me", isAuth, Controller.getMyData);
 
 // ---- User Login ---- //
-router.post("/login", validator(schema.login), AuthController.login);
+router.post("/login", validator(schema.login), Controller.login);
 
 // ---- User Register ---- //
-router.post("/register", validator(schema.register), AuthController.register);
+router.post("/register", validator(schema.register), Controller.register);
 
 module.exports = router;
